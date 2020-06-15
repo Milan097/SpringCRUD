3fda6ae593da313075fcae3104084cffba1369f0
Milan Thummar <thummarm097@gmail.com>
2020-06-15 10:25:33 +0530

    Development Branch

--- src/main/java/com/myApp/springCRUD/StudentRegister.java
+++ src/main/java/com/myApp/springCRUD/StudentRegister.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class StudentRegister {
 	// This is new Version
+	// This is new Developement Branch
 	public static void main(String[] args) {
+		System.out.println("Development Branch -> Push");
 		SpringApplication.run(StudentRegister.class, args);
 	}
 	
